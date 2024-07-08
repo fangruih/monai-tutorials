d1d25bf3e58ff50a33b80d6c5aa1fa162e109ee5
fangruih <fangruih@stanford.edu>
2024-07-08 06:51:11 +0000

    add test files

---
+++ generative/3d_ldm/metrics.py
@@ -0,0 +1,63 @@
+import numpy as np
+import torch
+from pytorch_msssim import ssim, ms_ssim
+import math 
+
+def calc_mses(pred, gt):
+    # pred: pytorch tensor of shape batch,x,y,z or x,y,z (squeeze before!)
+    # gt: pytorch tensor of shape batch,x,y,z or x,y,z (squeeze before!)
+    # returns an np array of shape batch
+    if len(pred.shape) == 3:
+        assert len(gt.shape) == 3, f'Ground-truth images to 3D PSNR calculation had to have a shape of (x,y,z) but has {gt.shape}'
+        mses = ((pred-gt)**2).mean().detach().cpu().numpy().reshape(-1)
+    else:
+        assert len(pred.shape) == 4, f'Predictions to 3D PSNR calculation had to have a shape of (batch,x,y,z) but has {pred.shape}'
+        assert len(gt.shape) == 4, f'Ground-truth images to 3D PSNR calculation had to have a shape of (batch,x,y,z) but has {gt.shape}'
+        assert pred.shape[0] == gt.shape[0], f'Predictions and ground-truth images have different batch sizes: {pred.shape[0]}, {gt.shape[0]}'
+        mses = ((pred-gt)**2).mean((1,2,3)).detach().cpu().numpy() # an np array of shape batch
+    return mses
+
+def calc_3d_psnrs_from_mses(mses):
+    # mses: numpy array of shape batch (calculated from pred & gt)
+    # NOTE: assumes that gt had the range [0,1]! Thus, 20log(1)=0!
+    return -10 * np.log10(mses)
+
+def calc_ssims(pred, gt):
+    # pred: pytorch tensor of shape batch, ... (no need to squeeze)
+    # gt: pytorch tensor of shape batch, ... (no need to squeeze)
+    # returns an np array of shape batch
+    # NOTE: assumes that gt has the range [0,1]!
+    assert pred.shape[0] == gt.shape[0], 'Batch sizes of prediction and gt are different in ssim calculation!'
+    ssim_val = ssim(pred, gt, data_range=1, size_average=False) # return (N,)
+    return ssim_val.detach().cpu().numpy()
+
+def calc_ms_ssims(pred, gt):
+    # pred: pytorch tensor of shape batch, ... (no need to squeeze)
+    # gt: pytorch tensor of shape batch, ... (no need to squeeze)
+    # returns an np array of shape batch
+    # NOTE: assumes that gt has the range [0,1]!
+    assert pred.shape[0] == gt.shape[0], 'Batch sizes of prediction and gt are different in ms-ssim calculation!'
+    ms_ssim_val = ms_ssim(pred, gt, data_range=1, size_average=False ) #(N,)
+    return ms_ssim_val.detach().cpu().numpy()
+
+
+def calc_mmd(pred, gt):
+    # pred: pytorch tensor of shape batch, ... (no need to squeeze)
+    # gt: pytorch tensor of shape batch, ... (no need to squeeze)
+    # returns a number
+    # if you will take an average, weigh the dist with the batch size in case of different batch sizes
+    # NOTE: MMD estimation requires great batch sizes to approximate it better! Save predictions and calculate MMD at the end.
+    assert pred.shape[0] == gt.shape[0], 'Batch sizes of prediction and gt are different in mmd calculation!'
+    batch = pred.shape[0]
+    x = pred.view(batch, -1)
+    y = gt.view(batch, -1)
+
+    xx, yy, zz = torch.mm(x,x.t()), torch.mm(y,y.t()), torch.mm(x,y.t())
+    xx = xx - torch.diag(torch.diag(xx))
+    yy = yy - torch.diag(torch.diag(yy))
+
+    beta = (1./(batch*(batch-1)))
+    gamma = (2./(batch*batch)) 
+
+    dist = beta * (torch.sum(xx)+torch.sum(yy)) - gamma * torch.sum(zz)
+    return math.sqrt(dist.item()) # number
---
+++ generative/3d_ldm/train_autoencoder_custom_test.py
@@ -0,0 +1,127 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import json
+import logging
+
+import os
+import sys
+from pathlib import Path
+
+import torch
+from generative.losses import PatchAdversarialLoss, PerceptualLoss
+from generative.networks.nets import PatchDiscriminator
+from monai.config import print_config
+from monai.utils import set_determinism
+from torch.nn import L1Loss, MSELoss
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.tensorboard import SummaryWriter
+
+from utils import KL_loss, define_instance, prepare_dataloader, prepare_dataloader_extract_dataset, prepare_dataloader_extract_dataset_custom, setup_ddp, print_gpu_memory
+from test_utils import *
+from visualize_image import visualize_one_slice_in_3d_image
+from create_dataset import *
+
+
+def main():
+    parser = argparse.ArgumentParser(description="PyTorch VAE-GAN training")
+    parser.add_argument(
+        "-e",
+        "--environment-file",
+        default="./config/environment.json",
+        help="environment json file that stores environment path",
+    )
+    parser.add_argument(
+        "-c",
+        "--config-file",
+        default="./config/config_train_32g.json",
+        help="config json file that stores hyper-parameters",
+    )
+    parser.add_argument("-g", "--gpus", default=1, type=int, help="number of gpus per node")
+    args = parser.parse_args()
+
+    # Step 0: configuration
+    ddp_bool = args.gpus > 1  # whether to use distributed data parallel
+    if ddp_bool:
+        rank = int(os.environ["LOCAL_RANK"])
+        world_size = int(os.environ["WORLD_SIZE"])
+        dist, device = setup_ddp(rank, world_size)
+    else:
+        rank = 0
+        world_size = 1
+        device = 0
+
+    torch.cuda.set_device(device)
+    print(f"Using {device}")
+
+    print_config()
+    torch.backends.cudnn.benchmark = True
+    torch.set_num_threads(4)
+    torch.autograd.set_detect_anomaly(True)
+
+    env_dict = json.load(open(args.environment_file, "r"))
+    config_dict = json.load(open(args.config_file, "r"))
+
+    for k, v in env_dict.items():
+        setattr(args, k, v)
+    for k, v in config_dict.items():
+        setattr(args, k, v)
+
+    set_determinism(42)
+
+    
+    base_dir = '/home/sijun/meow/data_new/hcp/registered'
+    # hcp_dataset, _ = create_train_val_datasets(base_dir)
+    
+    
+    
+    # Step 1: set data loader
+    size_divisible = 2 ** (len(args.autoencoder_def["num_channels"]) - 1)
+    hcp_dataset =  prepare_dataloader_extract_dataset_custom(
+        args,
+        args.autoencoder_train["batch_size"],
+        args.autoencoder_train["patch_size"],
+        base_dir= base_dir,
+        randcrop=True,
+        rank=rank,
+        world_size=world_size,
+        cache=1.0,
+        download=False,
+        size_divisible=size_divisible,
+        amp=False,
+    )
+    print("done hcp")
+    decathlon_dataset= prepare_dataloader_extract_dataset(
+        args,
+        args.autoencoder_train["batch_size"],
+        args.autoencoder_train["patch_size"],
+        randcrop=True,
+        rank=rank,
+        world_size=world_size,
+        cache=1.0,
+        download=False,
+        size_divisible=size_divisible,
+        amp=False,
+    )
+    
+    
+    
+    print("done decathlon ")
+    
+    
+    
+    compare_datasets(decathlon_dataset, decathlon_dataset)
+    print("done compare")
+
+if __name__ == "__main__":
+    
+    main()
